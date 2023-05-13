c4a2c8683de45ef98400e1286c9faacf9950bfa4
mdellabani <mde@activeviam.com>
2023-05-13 18:51:14 +0200

    contact section

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -377,7 +377,7 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: '',
   items: [
     {
       type: ContactType.Email,
