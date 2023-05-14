11b26c37cb6282445e19b8d247742f2fc03f785e
mdellabani <mde@activeviam.com>
2023-05-14 14:53:46 +0200

    lint

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,6 +1,6 @@
 import {ChevronUpIcon} from '@heroicons/react/outline';
-import {FC, memo} from 'react';
 import Link from 'next/link';
+import {FC, memo} from 'react';
 
 import {SectionId} from '../../data/data';
 import Socials from '../Socials';
@@ -8,9 +8,8 @@ import Socials from '../Socials';
 const Footer: FC = memo(() => (
   <div className="relative bg-neutral-900 px-4 pb-6 pt-12 sm:px-8 sm:pb-8 sm:pt-14">
     <div className="absolute inset-x-0 -top-4 flex justify-center sm:-top-6">
-    <Link href={`/#${SectionId.Hero}`} passHref>
-        <a
-          className="rounded-full bg-neutral-100 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2">
+      <Link href={`/#${SectionId.Hero}`} passHref>
+        <a className="rounded-full bg-neutral-100 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2">
           <ChevronUpIcon className="h-6 w-6 bg-transparent sm:h-8 sm:w-8" />
         </a>
       </Link>
