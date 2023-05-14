6c7dfe77875b7245b6fccc61f238f0ae09b33c11
mdellabani <mde@activeviam.com>
2023-05-14 15:06:19 +0200

    chevonDown wrap into Link

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -6,6 +6,7 @@ import {FC, memo} from 'react';
 import {heroData, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 import Socials from '../Socials';
+import Link from 'next/link';
 
 const Hero: FC = memo(() => {
   const {imageSrc, name, description, actions} = heroData;
@@ -49,11 +50,13 @@ const Hero: FC = memo(() => {
           </div>
         </div>
         <div className="absolute inset-x-0 bottom-6 flex justify-center">
-          <a
-            className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
-            href={`/#${SectionId.About}`}>
-            <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
-          </a>
+         <Link href={`/#${SectionId.Hero}`} passHref>
+            <a
+              className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
+              href={`/#${SectionId.About}`}>
+              <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
+            </a>
+          </Link>
         </div>
       </div>
     </Section>
