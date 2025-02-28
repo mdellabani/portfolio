12674c52932543af32a38abb5d83a4c8e7c5e4ac
mdellabani <mahieddinedellabani-parteners@huawei.com>
2025-02-28 21:33:55 +0100

    update font loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
+import {Chakra_Petch} from "next/font/google";
 import {memo} from 'react';
 
+const chakraPetch = Chakra_Petch({weight: ["400", "700"], subsets: ["latin"]});
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
-    <>
+        <main className={chakraPetch.className}>
+
       <Component {...pageProps} />
-    </>
+        </main>
   );
 });
 
