7cfd7a459a82dc2f4b295db422852d3a70600fbb
mdellabani <mdellabani@users.noreply.github.com>
2026-02-20 23:44:06 +0000

    deploy: 088503e6cc10167a27211792b497e908a1bfe817

---
+++ _next/static/WyiIFHjBB_fMHiu9hPc87/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/652-38c361455bc3c6bf.js","static/chunks/pages/index-a9099d0ab77d538d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
